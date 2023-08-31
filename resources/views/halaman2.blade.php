2ef4a140194baf544798136168022f5838e11b63
aminhnh <109867158+aminhnh@users.noreply.github.com>
2023-08-31 14:33:07 +0700

    Initial commit

---
+++ resources/views/halaman2.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Lain</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>Ini merupakan halaman 2</h1>
+    <br><br>
+    <h2>Dibawah ini merupakan </h2>
+    <h3> {{$name}} </h3>
+    <h3> {{$email}} </h3>
+    <script src="js/script.js"></script>
+</body>
+</html>
